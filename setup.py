fdfd7267bccf5e79ce187ddf4485e2042ce36efc
Alexander Loechel <Alexander.Loechel@lmu.de>
2016-11-22 13:41:07 +0100

    renames

--- setup.py
+++ setup.py
@@ -4,9 +4,9 @@ from setuptools import setup
 from setuptools import find_packages
 
 here = os.path.abspath(os.path.dirname(__file__))
-with open(os.path.join(here, 'README.txt')) as f:
+with open(os.path.join(here, 'README.rst')) as f:
     README = f.read()
-with open(os.path.join(here, 'CHANGES.txt')) as f:
+with open(os.path.join(here, 'CHANGES.rst')) as f:
     CHANGES = f.read()
 
 
